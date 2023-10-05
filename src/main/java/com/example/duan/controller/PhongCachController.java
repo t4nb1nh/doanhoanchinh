739772e6fe6ccf0a9a39ec8e3720401ce2ec44d2
GiangVTPh27185 <118868096+GiangVTPh27185@users.noreply.github.com>
2023-10-05 20:28:04 +0700

    test1 abcd

--- src/main/java/com/example/duan/controller/PhongCachController.java
+++ src/main/java/com/example/duan/controller/PhongCachController.java
@@ -1,4 +1,17 @@
 package com.example.duan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/PhongCach")
 public class PhongCachController {
+
+    @GetMapping
+    public String index(Model model) {
+
+        return "PhongCach/Index";
+    }
 }
