a46a241c442fceda97a63009e9cba8ffc4f4f246
t4nb1nh <binhhtph27077@fpt.edu.vn>
2023-11-01 22:23:51 +0700

    commit

---
+++ src/main/java/com/example/duan/Entity/ChatLieu.java
@@ -0,0 +1,33 @@
+package com.example.duan.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "chatlieu")
+public class ChatLieu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idchatlieu")
+    private Integer idChatLieu;
+    @Column(name = "ma")
+    private String ma;
+    @Column(name = "ten")
+    private String ten;
+    @Column(name = "trangthai")
+    private boolean trangThai;
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "ngaytao")
+    private Date ngayTao;
+}
