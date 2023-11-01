4a6baf8e97fd0568dd05541713f30a5bc926e851
t4nb1nh <binhhtph27077@fpt.edu.vn>
2023-11-02 02:27:20 +0700

    commit

--- src/main/java/com/example/duan/Controller/ThongKeController.java
+++ src/main/java/com/example/duan/Controller/ThongKeController.java
@@ -5,6 +5,7 @@ import com.example.duan.entity.StatusValue;
 import com.example.duan.service.HoaDonService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
@@ -24,19 +25,21 @@ import java.util.List;
 public class ThongKeController {
     @Autowired
     private HoaDonService hoaDonService;
-
     @Autowired
     HttpServletRequest request;
 
     @GetMapping({"/hoa-don"})
     public String listFirstPage(@RequestParam(name = "status", required = false) Integer status, Model model) {
+
         HttpSession session = this.request.getSession();
 //        if (session.getAttribute("admin") == null)
 //            return "redirect:/login-admin";
-        if (status != null)
+        if (status != null) {
             return listByPageStatus(1, status.intValue(), model, "ngayTao", "desc", null);
+        }
         return listByPage(1, model, "ngayTao", "desc", null);
     }
+
     @GetMapping("/hoa-don/{pageNum}")
     private String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model, @Param("sortField") String sortField, @Param("sortDir") String sortDir, @Param("keyword") String keyword) {
         Page<HoaDon> page = this.hoaDonService.listByPage(pageNum, sortField, sortDir, keyword);
@@ -44,8 +47,45 @@ public class ThongKeController {
         long startCount = ((pageNum - 1) * 10 + 1);
         long endCount = startCount + 10L - 1L;
 
-        if (endCount > page.getTotalElements()){
-            endCount = page.getTotalElements();}
+        if (endCount > page.getTotalElements()) {
+            endCount = page.getTotalElements();
+        }
+        List<StatusValue> statusValues = Arrays.asList(new StatusValue[]{new StatusValue(0, "Chờ giao hàng"), new StatusValue(1, "Đang chuẩn bị"), new StatusValue(2, "Đang giao hàng"), new StatusValue(3, "Hoàn thành"), new StatusValue(4, "đã hủy"), new StatusValue(5, "Yêu cầu hoàn trả"), new StatusValue(6, "Đã hoàn trả")});
+        model.addAttribute("statusValues", statusValues);
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+        model.addAttribute("currentPage", Integer.valueOf(pageNum));
+        model.addAttribute("totalPages", Integer.valueOf(page.getTotalPages()));
+        model.addAttribute("startCount", Long.valueOf(startCount));
+        model.addAttribute("endCount", Long.valueOf(endCount));
+        model.addAttribute("totalItem", Long.valueOf(page.getTotalElements()));
+        model.addAttribute("listDonHang", listDonHang);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", reverseSortDir);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("sumAll", this.hoaDonService.tongSoDonHang());
+        model.addAttribute("sumDHShipping", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(2)));
+        model.addAttribute("sumDHDone", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(3)));
+        model.addAttribute("sumDHCancel", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(4)));
+        model.addAttribute("sumDHReturn", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(6)));
+        return "ThongKe/ThongKehoadon";
+    }
+
+    @GetMapping({"/hoa-don/{status}/{pageNum}"})
+    private String listByPageStatus(@PathVariable(name = "pageNum") int pageNum, @PathVariable(name = "status") int status, Model model, @Param("sortField") String sortField, @Param("sortDir") String sortDir, @Param("keyword") String keyword) {
+        Page<HoaDon> page;
+        List<HoaDon> listDonHang;
+        HttpSession session = this.request.getSession();
+
+        if (status == 10) {
+            page = this.hoaDonService.listByPage(pageNum, sortField, sortDir, keyword);
+            listDonHang = page.getContent();
+        } else {
+            page = this.hoaDonService.listByPageStatus(pageNum, sortField, sortDir, keyword, status);
+            listDonHang = page.getContent();
+        }
+        long startCount = ((pageNum - 1) * 10 + 1);
+        long endCount = Math.min(startCount + 10L - 1L, page.getTotalElements());
         List<StatusValue> statusValues = Arrays.asList(new StatusValue[]{new StatusValue(0, "Chờ giao hàng"), new StatusValue(1, "Đang chuẩn bị"), new StatusValue(2, "Đang giao hàng"), new StatusValue(3, "Hoàn thành"), new StatusValue(4, "đã hủy"), new StatusValue(5, "Yêu cầu hoàn trả"), new StatusValue(6, "Đã hoàn trả")});
         String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
         model.addAttribute("statusValues", statusValues);
@@ -64,41 +104,7 @@ public class ThongKeController {
         model.addAttribute("sumDHDone", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(3)));
         model.addAttribute("sumDHCancel", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(4)));
         model.addAttribute("sumDHReturn", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(6)));
-        return "/thong-ke/hoa-don";
-    }
-    @GetMapping({"/hoa-don/{status}/{pageNum}"})
-    private String listByPageStatus(@PathVariable(name = "pageNum") int pageNum, @PathVariable(name = "status") int status, Model model, @Param("sortField") String sortField, @Param("sortDir") String sortDir, @Param("keyword") String keyword) {
-        Page<HoaDon> page;
-        HttpSession session = this.request.getSession();
-        List<HoaDon> listDonHang1 = new ArrayList<>();
-        if (status == 10) {
-            page = this.hoaDonService.listByPage(pageNum, sortField, sortDir, keyword);
-            listDonHang1 = page.getContent();
-        } else {
-            page = this.hoaDonService.listByPageStatus(pageNum, sortField, sortDir, keyword, status);
-            listDonHang1 = page.getContent();
-        }
-        long startCount = ((pageNum - 1) * 10 + 1);
-        long endCount = Math.min(startCount + 10L - 1L, page.getTotalElements());
-        List<StatusValue> statusValues = Arrays.asList(new StatusValue[]{new StatusValue(0, "Chờ giao hàng"), new StatusValue(1, "Đang chuẩn bị"), new StatusValue(2, "Đang giao hàng"), new StatusValue(3, "Hoàn thành"), new StatusValue(4, "đã hủy"), new StatusValue(5, "Yêu cầu hoàn trả"), new StatusValue(6, "Đã hoàn trả")});
-        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
-        model.addAttribute("statusValues", statusValues);
-        model.addAttribute("currentPage", Integer.valueOf(pageNum));
-        model.addAttribute("totalPages", Integer.valueOf(page.getTotalPages()));
-        model.addAttribute("startCount", Long.valueOf(startCount));
-        model.addAttribute("endCount", Long.valueOf(endCount));
-        model.addAttribute("totalItem", Long.valueOf(page.getTotalElements()));
-        model.addAttribute("listDonHang", listDonHang1);
-        model.addAttribute("sortField", sortField);
-        model.addAttribute("sortDir", sortDir);
-        model.addAttribute("reverseSortDir", reverseSortDir);
-        model.addAttribute("keyword", keyword);
-        model.addAttribute("sumAll", this.hoaDonService.tongSoDonHang());
-        model.addAttribute("sumDHShipping", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(2)));
-        model.addAttribute("sumDHDone", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(3)));
-        model.addAttribute("sumDHCancel", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(4)));
-        model.addAttribute("sumDHReturn", this.hoaDonService.soDonHangTheoTrangThai(Integer.valueOf(6)));
         model.addAttribute("selectedStatus", Integer.valueOf(status));
-        return "/thong-ke/hoa-don";
-  }
+        return "ThongKe/ThongKehoadon";
+    }
 }
--- src/main/java/com/example/duan/Repository/HoaDonRepository.java
+++ src/main/java/com/example/duan/Repository/HoaDonRepository.java
@@ -13,7 +13,7 @@ public interface HoaDonRepository extends JpaRepository<HoaDon, Integer> {
     Integer tongSoDonHang();
     @Query("SELECT count(hd) FROM HoaDon hd WHERE hd.trangThai = ?1")
     Integer soDonHangTheoTrangThai(Integer trangThai);
-    @Query("SELECT d\n    FROM HoaDon d\n")
+    @Query("SELECT dn    FROM HoaDon dn")
     Page<HoaDon> findAll(Pageable paramPageable);
     @Query("SELECT dh FROM HoaDon dh WHERE UPPER(CONCAT( dh.maHoaDon, ' ', dh.khachHang.ten, '')) LIKE %?1% ")
     Page<HoaDon> findAllPagination(String paramString, Pageable paramPageable);
