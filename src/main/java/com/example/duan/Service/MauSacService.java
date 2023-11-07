273252d17656f3d468f4e5c70c1559c425c66d61
t4nb1nh <binhhtph27077@fpt.edu.vn>
2023-11-07 17:22:34 +0700

    update

--- src/main/java/com/example/duan/Service/MauSacService.java
+++ src/main/java/com/example/duan/Service/MauSacService.java
@@ -1,21 +1,43 @@
 package com.example.duan.service;
 
 import com.example.duan.entity.MauSac;
+import com.example.duan.repository.MauSacRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+@Service
+public class MauSacService {
+    @Autowired
+    private MauSacRepository MauSacRepository;
 
-public interface MauSacService {
-    List<MauSac> findAll();
+    public List<MauSac> getAll(){
+        return MauSacRepository.findAll();
+    };
 
-    void saveMauSac(MauSac mauSac);
+    public MauSac addMauSac(MauSac MauSac) {
+        return MauSacRepository.save(MauSac);
+    }
 
-    MauSac findByID(Integer id);
+    public MauSac getById(int id) {
+        return MauSacRepository.findById(id).orElse(null);
+    }
 
-    MauSac update(MauSac mauSac);
+    public void updateMauSac(int Id, MauSac MauSac) {
+        Optional<MauSac> existingMauSac = MauSacRepository.findById(Id);
 
-    void delete(Integer id);
+        if (existingMauSac.isPresent()) {
+            MauSac updatedMauSac = existingMauSac.get();
+            updatedMauSac.setMa(MauSac.getMa());
+            updatedMauSac.setTen(MauSac.getTen());
+            updatedMauSac.setNgayTao(MauSac.getNgayTao());
+            updatedMauSac.setTrangThai(MauSac.isTrangThai());
+            MauSacRepository.save(updatedMauSac);
+        }
+    }
 
-    List<MauSac> findMauSacByMa(String ma);
+    public void deleteMauSac(int id) {
+        MauSacRepository.deleteById(id);
+    }
 }
